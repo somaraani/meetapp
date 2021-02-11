5673a5399e4c5ebd99f725b094ef7dfef9910669
ArshanK <arshan99khan@gmail.com>
2021-02-10 23:45:43 -0500

    added all types

--- shared/types.ts
+++ shared/types.ts
@@ -1,3 +1,23 @@
+export interface Meeting {
+    id: string,
+    status: string,
+    meetingDetails: MeetingDetail,
+    meetingParticipants: MeetingParticipant[]
+}
+
+export interface MeetingDetail {
+    description: string,
+    ownerId: string,
+    originalEta: string,
+    currentEta: string,
+    meetingLocation: Coordinate,   
+}
+
+export interface MeetingParticipant {
+    userId: string,
+    journeyId: string
+}
+
 export interface User {
     id: string,
     email: string,
@@ -19,4 +39,32 @@ export interface Invitation {
     meetingID: string,
     userID: string,
     status: string
+}
+
+export interface Journey {
+    id: string, 
+    meetingId: string,
+    startLocation: Coordinate,
+    eta: string,
+    lastUpdated: string,
+    locations: Coordinate[],
+    path: Coordinate[],
+    journeySetting: JourneySetting
+}
+
+export interface JourneySetting {
+    transitType: string,
+    tolls: boolean
+}
+
+export interface Coordinate {
+    long: number,
+    lat: number
+}
+
+export interface Notification {
+    notificationId: string,
+    userId: string,
+    title: string,
+    message: string
 }
