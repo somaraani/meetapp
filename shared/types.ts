deee8e3befefd548f12cfa5c2032c6b930c2fac3
Edward <edwardliu321@gmail.com>
2021-02-27 01:04:59 -0500

    navigation service

--- shared/types.ts
+++ shared/types.ts
@@ -53,12 +53,25 @@ export interface Journey {
 }
 
 export interface JourneySetting {
-    transitType: string,
-    tolls: boolean
+    transitMode?: TransitMode[],
+    avoid?: TravelRestriction[]
+}
+
+export enum TransitMode {
+    bus = "bus",
+    subway = "subway",
+    train = "train",
+    tram = "tram",
+    rail = "rail"
+}
+
+export enum TravelRestriction {
+    tolls = "tolls",
+    highways = "highways",
 }
 
 export interface Coordinate {
-    long: number,
+    lng: number,
     lat: number
 }
 
@@ -68,4 +81,10 @@ export interface Notification {
     title: string,
     message: string,
     read: boolean
+}
+
+export interface DirectionResponse {
+    path: Coordinate[],
+    eta: string,
+    distance: number
 }
