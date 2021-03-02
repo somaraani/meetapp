829e6608c38961cb69d41c0429d720202b5ec845
paperwarfare3@gmail.com <paperwarfare3@gmail.com>
2021-03-02 00:04:34 -0500

    adding axios wrapper

--- client/api/ApiWrapper.tsx
+++ client/api/ApiWrapper.tsx
@@ -1,172 +1,94 @@
 import {PublicUserData, User} from "@types"
-import { Value } from "react-native-reanimated";
-
+const axios = require('axios');
 const API_URL = 'http://localhost:3000/';
 
-function ApiError(message, data, status) {
-  let response = null;
-  let isObject = false;
-
-  try {
-    response = JSON.parse(data);
-    isObject = true;
-  } catch (e) {
-    response = data;
-  }
-
-  this.response = response;
-  this.message = message;
-  this.status = status;
-  this.toString = function () {
-    return `${ this.message }\nResponse:\n${ isObject ? JSON.stringify(this.response, null, 2) : this.response }`;
-  };
-}
-
-const fetchResource = (path, userOptions = {}) => {
-  const defaultOptions = {};
-  const defaultHeaders = {};
-
-  const options = {
-    ...defaultOptions,
-    ...userOptions,
-    headers: {
-      ...defaultHeaders,
-      ...userOptions.headers,
-    },
-  };
-
-  const url = `${ API_URL }/${ path }`;
-
-  const isFile = options.body instanceof File;
-
-  if (options.body && typeof options.body === 'object' && !isFile) {
-    options.body = JSON.stringify(options.body);
-  }
-
-  let response = null;
-
-  return fetch(url, options)
-    .then(responseObject => {
-      response = responseObject;
-
-      if (response.status === 401) {
-      }
-
-      if (response.status < 200 || response.status >= 300) {
-        return response.text();
-      }
-
-      return response.json();
-    })
-    .then(parsedResponse => {
-      if (response.status < 200 || response.status >= 300) {
-        throw parsedResponse;
-      }
-
-      return parsedResponse;
-    })
-    .catch(error => {
-      if (response) {
-        throw new ApiError(`Request failed with status ${ response.status }.`, error, response.status);
-      } else {
-        throw new ApiError(error.toString(), null, 'REQUEST_FAILED');
-      }
-    });
-};
-
-
 class ApiWrapper {
-
-    token:string;
-    id:string;
-
-    signIn(email:string, password:string): void {
-        fetchResource('authenticate/', {
-            method: 'POST',
-            body: {email, password},
-      
-        }).then(value => (this.token) = value);
-
-        fetchResource(`users/self/`, {
-          method: "GET",
-          headers: {
-            'Authorization': `Bearer ${this.token}`,
-          },
-        }).then(value => (this.id) = value);
-    }
-
-    signOut(): void {
-        this.token = null;
-        this.id = null;
-    }
-
-    isSignedIn():boolean {
-        if (this.token) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    createUser(email:string, password:string, details:PublicUserData): Promise<User> {
-        return fetchResource('users/', {
-          method: 'POST',
-          body: {email, password, details},
-      
-        });
-    }
-
-    updateUser(user:User): Promise<User> {
-        return fetchResource(`users/${ this.id }/`, {
-          method: "PUT",
-          body: user,
-          headers: {
-            'Authorization': `Bearer ${this.token}`,
-          },
-        });
-    }
-
-    getUser(id:string): Promise<User> {
-        return fetchResource(`users/${ id }/`, {
-          method: "GET",
-          headers: {
-            'Authorization': `Bearer ${this.token}`,
-          },
-        });
-    }
-
-    getSelfUserId(): Promise<User> {
-      return fetchResource(`users/self/`, {
-        method: "GET",
-        headers: {
-          'Authorization': `Bearer ${this.token}`,
-        },
-      });
+  constructor(){
+    this.token = "";
+    this.id = "";
   }
 
-    getPublicUser(id:string): Promise<PublicUserData> {
-        return fetchResource(`users/${ id}/public`, {
-          method: "GET",
-          headers: {
-            'Authorization': `Bearer ${this.token}`,
-          },
-        });
-    }
+  async signIn(email:string, password:string): Promise<void> {
+    let payload = { email: email, password: password};
+    let res = await axios.post(`${ API_URL }authenticate/`, payload);
+    let data = res.data;
+    this.token = data.access_token;    
+    let res2 = await axios.get(`${ API_URL }users/self/`, { headers: {"Authorization" : `Bearer ${this.token}`} });
+    this.id = (res2.data).id;
+  }
 
-    getUsers(userId_query:string, userEmail_query:string): Promise<User[]> {
-        var url = new URL('users/')
-        var params = {id:userId_query, email:userEmail_query}
-        url.search = new URLSearchParams(params).toString();
-      
-        return fetchResource(url, {
-          method: "GET",
-          headers: {
-            'Authorization': `Bearer ${this.token}`,
-          },
-        })
+  signOut(): void {
+    this.token = "";
+    this.id = "";
+  }
+
+  isSignedIn():boolean {
+    if (this.token) {
+      return true;
     }
+    else {
+      return false;
+    }
+  }
+
+  async createUser(email:string, password:string, details:PublicUserData): Promise<User> {
+    let payload = { email: email, password: password, details: details };
+    let res = await axios.post(`${ API_URL }users/`, payload);
+    let data = res.data;
+    return data;
+  }
+
+  async updateUser(user:User): Promise<User> {
+    let payload = { user: user };
+    let res = await axios.put(`${ API_URL }users/${ this.id }/`, payload, { headers: {"Authorization" : `Bearer ${this.token}`} });
+    let data = res.data;
+    return data;
+  }
+
+  async getUser(): Promise<User> {
+    let res = await axios.get(`${ API_URL }users/self/`, { headers: {"Authorization" : `Bearer ${this.token}`} });
+    let data = res.data;
+    console.log(data);
+    return data;
+  }
 
 }
 
-export default fetchResource;
+let api = new ApiWrapper();
+
+
+
+
+/* Create User Test - Passed
+api.createUser("nav66@gmail.com", "password", {displayName:"TESTACCOUNT55", displayPicture:"https://images.unsplash.com/photo-1535498051285-5613026fae05?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8ZGlzcGxheXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&w=1000&q=80"});
+*/
+
+/* Sign In Test - Passed
+api.signIn('nav66@gmail.com', 'password').then(()=>{
+  console.log(api.id);
+  console.log(api.token);
+});
+*/
+
+/* Testing isSignedIn() and signOut() - Passed
+console.log(api.isSignedIn());
+api.signIn('nav66@gmail.com', 'password').then(()=>{
+  console.log(api.isSignedIn());
+  api.signOut();
+  console.log(api.isSignedIn());
+});
+*/
+
+/* Testing updateUser - Failed
+api.signIn('nav66@gmail.com', 'password').then(()=>{
+  api.updateUser({THIS DID NOT WORK DOCUMENTATION DOES NOT LINE UP WITH THIS});
+});
+*/
+
+/* Testing getUser- Failed
+api.signIn('nav66@gmail.com', 'password').then(()=>{
+  api.getUser();
+});
+*/
+
+
