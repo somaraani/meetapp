9aba2babb61909d07d11db08c4508ab6a70311ad
Tasin Ahmed <atasin1998@gmail.com>
2021-03-05 23:30:33 -0500

    Create add meeting page

---
+++ client/screens/AddMeeting.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const AddMeeting = () => {
+  return (
+    <View>
+      <Text>Add Meeting</Text>
+    </View>
+  );
+};
+
+export default AddMeeting;
+
+const styles = StyleSheet.create({});
