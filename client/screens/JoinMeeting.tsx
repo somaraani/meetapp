41bced4532dfc1e24a2c78e8745b963948e1099a
Tasin Ahmed <atasin1998@gmail.com>
2021-03-06 20:51:02 -0500

    Create join meeting page

---
+++ client/screens/JoinMeeting.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const JoinMeeting = () => {
+  return (
+    <View>
+      <Text>Join an existing meeting</Text>
+    </View>
+  );
+};
+
+export default JoinMeeting;
+
+const styles = StyleSheet.create({});
