3401f852942a77ad47b9e5238889d3ac3d41353b
Tasin Ahmed <atasin1998@gmail.com>
2021-03-07 14:29:47 -0500

    Added header button

--- client/screens/CreateMeeting.tsx
+++ client/screens/CreateMeeting.tsx
@@ -1,7 +1,29 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const CreateMeeting = () => {
+
+const CreateMeeting = ({ navigation }) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#2196F3",
+            paddingVertical: 5,
+            paddingHorizontal: 10,
+            borderRadius: 8,
+          }}
+          onPress={() => }
+        >
+          <Text style={{ color: "white", fontWeight: "bold" }}>CREATE</Text>
+        </TouchableOpacity>
+      ),
+      headerRightContainerStyle: {
+        marginRight: 20,
+      },
+    });
+  }, []);
+
   return (
     <View>
       <Text>Create a new meeting</Text>
--- client/screens/JoinMeeting.tsx
+++ client/screens/JoinMeeting.tsx
@@ -1,7 +1,27 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
-const JoinMeeting = () => {
+const JoinMeeting = ({ navigation }) => {
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#2196F3",
+            paddingVertical: 5,
+            paddingHorizontal: 10,
+            borderRadius: 8,
+          }}
+        >
+          <Text style={{ color: "white", fontWeight: "bold" }}>JOIN</Text>
+        </TouchableOpacity>
+      ),
+      headerRightContainerStyle: {
+        marginRight: 20,
+      },
+    });
+  }, []);
+
   return (
     <View>
       <Text>Join an existing meeting</Text>
