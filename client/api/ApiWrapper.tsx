4a2f95eb63c6705e7a5be09cf5e8aa108d826667
Tasin Ahmed <atasin1998@gmail.com>
2021-03-08 00:51:58 -0500

    Set token to previouly logged in user

--- client/api/ApiWrapper.tsx
+++ client/api/ApiWrapper.tsx
@@ -1,3 +1,4 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import {
   Coordinate,
   Meeting,
@@ -13,6 +14,15 @@ export class ApiWrapper {
   public token: string;
   private id: string;
   constructor() {
+    AsyncStorage.getItem("user").then(async (token) => {
+      if (token) {
+        this.token = token;
+        this.id = (await this.getUser()).id;
+      } else {
+        this.token = "";
+        this.id = "";
+      }
+    });
     this.token = "";
     this.id = "";
   }
@@ -107,10 +117,10 @@ export class ApiWrapper {
     return data;
   }
 
-  async getMeetingsByUserId(userId: string): Promise<Meeting[]> {
+  async getMeetings(): Promise<Meeting[]> {
     let res = await axios.get(`${API_URL}meetings/`, {
       headers: { Authorization: `Bearer ${this.token}` },
-      params: { userId: userId },
+      params: { userId: this.id },
     });
     let data = res.data;
     return data;
