957be271a525c3446ca155514d8f9f6e55bb4f4f
Tasin Ahmed <atasin1998@gmail.com>
2021-03-09 20:16:46 -0500

    Show recently created meetings first

--- client/screens/Home.tsx
+++ client/screens/Home.tsx
@@ -8,35 +8,6 @@ import { AuthNavProps } from "../src/AuthParamList";
 
 const numColumns = 2;
 
-const MeetingsData = [
-  {
-    id: "1",
-    title: "Meeting 1",
-    latitude: 43.653225,
-    longitude: -79.383186,
-    members: [
-      {
-        latitude: 43.799554407720585,
-        longitude: -79.35446070585937,
-        mode: "BICYCLING",
-        distance: null,
-        eta: null,
-      },
-      {
-        latitude: 43.719188449399326,
-        longitude: -79.58128653338825,
-        mode: "WALKING",
-        distance: null,
-        eta: null,
-      },
-    ],
-  },
-  { id: "2", title: "Meeting 2", latitude: 28.599171, longitude: -81.201653 },
-  { id: "3", title: "Meeting 3", latitude: 40.712776, longitude: -74.005974 },
-  { id: "4", title: "Meeting 4", latitude: 48.858372, longitude: 2.294481 },
-  { id: "5", title: "Meeting 5", latitude: 43.1065603, longitude: -79.0639039 },
-];
-
 const formatData = (data: Meeting[], numColumns: any) => {
   const numberOfFullRows = Math.floor(data.length / numColumns);
 
@@ -67,7 +38,8 @@ const Home = ({ navigation }: AuthNavProps<"Home">) => {
   useEffect(() => {
     async function fetchMeetings() {
       try {
-        setMeetings(await getMeetings());
+        let meetingList = await getMeetings();
+        setMeetings(meetingList.reverse());
         // console.log(meetings);
       } catch (error) {
         console.log(error);
