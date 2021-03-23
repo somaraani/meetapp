178e0ea08de363c6a5b02c46071c8fbc6069a09c
Tasin Ahmed <atasin1998@gmail.com>
2021-03-23 18:41:00 -0400

    Add device back button action

--- client/screens/MeetingSettings.tsx
+++ client/screens/MeetingSettings.tsx
@@ -1,7 +1,21 @@
+import { useFocusEffect } from "@react-navigation/core";
 import React from "react";
-import { Text, View } from "react-native";
+import { BackHandler, Text, View } from "react-native";
 
-const MeetingSettings = () => {
+const MeetingSettings = ({navigation}) => {
+  const backAction = () => {
+    navigation.navigate("Home");
+    return true;
+  }
+
+  useFocusEffect(
+    React.useCallback(() => {
+    BackHandler.addEventListener("hardwareBackPress", backAction);
+    return () => {
+      BackHandler.addEventListener("hardwareBackPress", backAction);
+    }
+    }, []));
+
   return (
     <View>
       <Text>Meeting Settings</Text>
