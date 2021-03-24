1f44358b17cbf0414aa3a221e02a69803ab3f75d
Tasin Ahmed <atasin1998@gmail.com>
2021-03-24 16:13:53 -0400

    Show meeting name and sender name

--- client/screens/Invites.tsx
+++ client/screens/Invites.tsx
@@ -1,7 +1,7 @@
 import { useFocusEffect } from "@react-navigation/core";
 import { SocketEvents } from "@types";
 import React, { useContext, useState } from "react";
-import { BackHandler, StyleSheet, Text, View } from "react-native";
+import { BackHandler, StyleSheet, Text, View, ScrollView } from "react-native";
 import { Button, Icon, ListItem } from "react-native-elements";
 import { ApiContext } from "../src/ApiProvider";
 
@@ -9,11 +9,26 @@ const Invites = ({ navigation }) => {
   const { apiClient, socketClient } = useContext(ApiContext);
   const [invites, setInvites] = useState([]);
 
-  async function getMeetingData(id) {
-    let data = await apiClient.getMeeting(id);
-    return data;
+  async function getInvites() {
+    try {
+      let user = await apiClient.getUser();
+      let data = await apiClient.getInvitations({ userId: user.id });
+      let allData = data.filter((x) => x.status === "pending");
+      for (let i = 0; i < allData.length; i++) {
+        try {
+          let meetData = await apiClient.getMeeting(allData[i].meetingId);
+          let sender = await apiClient.getPublicUser(meetData.ownerId);
+          console.log(meetData.ownerId);
+          allData[i] = { ...allData[i], meetData, sender };
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      setInvites(allData);
+    } catch (error) {
+      console.log(error);
+    }
   }
-
   const backAction = () => {
     navigation.navigate("Home");
     return true;
@@ -22,6 +37,7 @@ const Invites = ({ navigation }) => {
   const acceptInvite = async (x) => {
     try {
       let data = await apiClient.updateInvitation(x.id, true);
+      getInvites();
     } catch (error) {
       console.log(error);
     }
@@ -30,6 +46,7 @@ const Invites = ({ navigation }) => {
   const declineInvite = async (x) => {
     try {
       let data = await apiClient.updateInvitation(x.id, false);
+      getInvites();
     } catch (error) {
       console.log(error);
     }
@@ -38,11 +55,6 @@ const Invites = ({ navigation }) => {
   useFocusEffect(
     React.useCallback(() => {
       BackHandler.addEventListener("hardwareBackPress", backAction);
-      async function getInvites() {
-        let user = await apiClient.getUser();
-        let data = await apiClient.getInvitations({ userId: user.id });
-        setInvites(data);
-      }
       getInvites();
       socketClient.on(SocketEvents.INVITATION, () => {
         console.log("New invite");
@@ -64,25 +76,33 @@ const Invites = ({ navigation }) => {
 
   return (
     <View>
-      {invites.map((x, i) => (
-        <ListItem key={i} bottomDivider>
-          <ListItem.Content>
-            <ListItem.Title>{x.meetingId}</ListItem.Title>
-          </ListItem.Content>
-          <View style={{ flexDirection: "row" }}>
-            <Button
-              icon={<Icon name="check" color="white" size={18} />}
-              containerStyle={{ marginRight: 5 }}
-              onPress={() => acceptInvite(x)}
-            />
-            <Button
-              buttonStyle={{ backgroundColor: "#F66161" }}
-              icon={<Icon name="close" color="white" size={18} />}
-              onPress={() => declineInvite(x)}
-            />
-          </View>
-        </ListItem>
-      ))}
+      <ScrollView>
+        {invites.map((x, i) => (
+          <ListItem key={i} bottomDivider>
+            <ListItem.Content>
+              <ListItem.Title>
+                Invitation to join{" "}
+                <Text style={{ fontWeight: "bold" }}>
+                  {x.meetData.details.name}
+                </Text>
+              </ListItem.Title>
+              <ListItem.Subtitle>From {x.sender.displayName}</ListItem.Subtitle>
+            </ListItem.Content>
+            <View style={{ flexDirection: "row" }}>
+              <Button
+                icon={<Icon name="check" color="white" size={18} />}
+                containerStyle={{ marginRight: 5 }}
+                onPress={() => acceptInvite(x)}
+              />
+              <Button
+                buttonStyle={{ backgroundColor: "#F66161" }}
+                icon={<Icon name="close" color="white" size={18} />}
+                onPress={() => declineInvite(x)}
+              />
+            </View>
+          </ListItem>
+        ))}
+      </ScrollView>
     </View>
   );
 };
