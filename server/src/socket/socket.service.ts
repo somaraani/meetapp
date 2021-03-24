7e5404ea513c1cfb6b9cb30f20dfb098106e5bdb
Tasin Ahmed <atasin1998@gmail.com>
2021-03-24 01:48:34 -0400

    Fix formatting

--- server/src/socket/socket.service.ts
+++ server/src/socket/socket.service.ts
@@ -4,57 +4,56 @@ import { SocketEvents } from '@types';
 import { Server, Socket } from 'socket.io';
 
 @Injectable()
-export class SocketService{
-    private server: Server;
-    private connections :   { [userId: string] : Socket }
-    private socketUserMap : { [socketId: string] : string}
-    constructor(){
-        this.connections = {};
-        this.socketUserMap = {};
-    }
+export class SocketService {
+  private server: Server;
+  private connections: { [userId: string]: Socket };
+  private socketUserMap: { [socketId: string]: string };
+  constructor() {
+    this.connections = {};
+    this.socketUserMap = {};
+  }
 
-    public addConnection(userId : string, socket: Socket){
-        this.connections[userId] = socket;
-        this.socketUserMap[socket.id] = userId;
-    }
+  public addConnection(userId: string, socket: Socket) {
+    this.connections[userId] = socket;
+    this.socketUserMap[socket.id] = userId;
+  }
 
-    public setServer(server: Server){
-        this.server = server;
-    }
-   
-    public removeConnection(socket: Socket){
-        const userId = this.socketUserMap[socket.id];
-        delete this.connections[userId]
-        delete this.socketUserMap[socket.id];
-    }
+  public setServer(server: Server) {
+    this.server = server;
+  }
 
-    public emitToUser(userId:string, event:SocketEvents, message: any) {
-        if (this.isConnected(userId)){
-            this.connections[userId].emit(event, message);
-        }
-    }
+  public removeConnection(socket: Socket) {
+    const userId = this.socketUserMap[socket.id];
+    delete this.connections[userId];
+    delete this.socketUserMap[socket.id];
+  }
 
-    public emitToRoom(roomId:string, event:SocketEvents, message?:any) {
-        try{
-            this.server.to(roomId).emit(event, message);
-        }
-        catch (e){
-            console.log('could not emit to room:' + roomId);
-            console.log(e);
-        }
+  public emitToUser(userId: string, event: SocketEvents, message: any) {
+    if (this.isConnected(userId)) {
+      this.connections[userId].emit(event, message);
     }
+  }
 
-    public joinRoom(userId:string, roomId: string) {
-        const userSocket = this.connections[userId];
-        userSocket.join(roomId);
+  public emitToRoom(roomId: string, event: SocketEvents, message?: any) {
+    try {
+      this.server.to(roomId).emit(event, message);
+    } catch (e) {
+      console.log('could not emit to room:' + roomId);
+      console.log(e);
     }
-    
-    public leaveRoom(userId:string, roomId: string) {
-        const userSocket = this.connections[userId];
-        userSocket.leave(roomId);
-    }
+  }
 
-    public isConnected(userId:string) : boolean {
-        return this.connections[userId] ? true : false;
-    }
+  public joinRoom(userId: string, roomId: string) {
+    const userSocket = this.connections[userId];
+    userSocket.join(roomId);
+  }
+
+  public leaveRoom(userId: string, roomId: string) {
+    const userSocket = this.connections[userId];
+    userSocket.leave(roomId);
+  }
+
+  public isConnected(userId: string): boolean {
+    return this.connections[userId] ? true : false;
+  }
 }
