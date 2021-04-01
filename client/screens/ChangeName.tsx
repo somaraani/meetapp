e4b0f306273ad29d69ad30de8f9b2415d544a400
Tasin Ahmed <atasin1998@gmail.com>
2021-04-01 14:45:13 -0400

    Add input to change name

--- client/screens/ChangeName.tsx
+++ client/screens/ChangeName.tsx
@@ -1,29 +1,64 @@
-import React, { useEffect } from "react";
+import { useFocusEffect } from "@react-navigation/core";
+import React, { useContext, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
-import { Button } from "react-native-paper";
+import { Button, TextInput } from "react-native-paper";
+import { ApiContext } from "../src/ApiProvider";
 
 const ChangeName = ({ navigation }) => {
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Button
-          mode="contained"
-          onPress={() => navigation.navigate("Home")}
-          theme={{ colors: { primary: "#2196F3" } }}
-        >
-          <Text style={{ color: "white" }}>SAVE</Text>
-        </Button>
-      ),
-    });
-  }, []);
+  const [name, setName] = useState("");
+  const { apiClient } = useContext(ApiContext);
+
+  async function changeUserInfo() {
+    try {
+      let data = await apiClient.getUser();
+      await apiClient.updateUser({
+        ...data,
+        publicData: { ...data.publicData, displayName: name },
+      });
+
+      navigation.goBack();
+    } catch (error) {
+      console.log(error.response);
+    }
+  }
+
+  useFocusEffect(
+    React.useCallback(() => {
+      async function getName() {
+        let data = await apiClient.getUser();
+        setName(data.publicData.displayName);
+      }
+
+      getName();
+    }, [])
+  );
 
   return (
-    <View>
-      <Text>Change Name</Text>
+    <View style={{ padding: 10 }}>
+      <TextInput
+        style={styles.input}
+        placeholder="Full name"
+        onChangeText={(value) => setName(value)}
+        value={name}
+        theme={{ colors: { primary: "#2196F3" } }}
+        mode="outlined"
+      />
+      <Button
+        mode="contained"
+        onPress={changeUserInfo}
+        theme={{ colors: { primary: "#2196F3" } }}
+      >
+        <Text style={{ color: "white" }}>SAVE</Text>
+      </Button>
     </View>
   );
 };
 
 export default ChangeName;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 10,
+    height: 40,
+  },
+});
